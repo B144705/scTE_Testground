e1f5ef6d3969dbe57f465524b854705774d69fcc
B144705 <130005623+B144705@users.noreply.github.com>
2026-02-16 07:37:48 +0000

    Rename testground package and CLI scripts to avoid conflicts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
       with open('README.md',encoding="utf-8") as f:
         return f.read()
 
-setup(name='scTE',
+setup(name='scTE-testground',
         version='1.0',
         description='Tool for estimating differential enrichment of Transposable Elements and other highly repetitive regions in single-cell data',
         long_description=readme(),
@@ -34,9 +34,9 @@ setup(name='scTE',
         include_package_data=True,
         zip_safe=False,
         scripts=[
-          'bin/scTE',
-          'bin/scTE_build',
-          'bin/scTEATAC_build',
-          'bin/scTEATAC',
+          'bin/scTE_testground',
+          'bin/scTE_build_testground',
+          'bin/scTEATAC_build_testground',
+          'bin/scTEATAC_testground',
         ]
         )
